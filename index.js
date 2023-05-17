4e74d3cdaa707ec40741e092f09647ccaf89494e
StanMungai <stanfordkangethe@outlook.com>
2023-05-17 16:34:01 +0300

    Completed the looping lab

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+function writeCards(array, name) {
+    let msgArray = [];
+
+    for (let i = 0; i < array.length; i++) {
+        let msg = `Thank you, ${array[i]}, for the wonderful ${name} gift!`;
+        msgArray.push(msg)
+    }
+
+    return msgArray
+}
+
+
+function countDown(num) {
+    while (num >= 0) {
+        console.log(num)
+        num--
+    }
+}
